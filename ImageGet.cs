d5465e06e79f94943621343c91c750f477b5afdf
Lawrence Paine <lawrence.apine95@gmail.co.uk>
2021-03-14 21:52:11 +0000

    Adds Image get class and dependencies

---
+++ ImageGet.cs
@@ -0,0 +1,35 @@
+﻿using BlastOff.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.CosmosRepository;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace BlastOff.Api
+{
+    public class ImageGet
+    {
+        private readonly IRepository<Image> _imageRepository;
+
+        public ImageGet(IRepository<Image> imageRepository) => _imageRepository = imageRepository;
+
+        [FunctionName("ImageGet")]
+        public IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "image")] HttpRequest req,
+            ILogger log)
+        {
+            var hasDays = int.TryParse(req.Query["days"], out var days);
+            log.LogInformation($"Requested images from last {days} days.");
+
+            if (!hasDays && days > 90)
+                return new BadRequestResult();
+
+            var imageResponse = _imageRepository.GetAsync
+                (img => img.Date > DateTime.Now.AddDays(-days));
+
+            return new OkObjectResult(imageResponse.Result);
+        }
+    }
+}

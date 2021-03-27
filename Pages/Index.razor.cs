a775613d193c8673a147259c89bdea0d30fdc71f
Lawrence Paine <lawrence.apine95@gmail.co.uk>
2021-03-27 22:24:02 +0000

    added code behind link to index component

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,13 @@
+﻿using BlastOff.Shared;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Family_Dashboard.Pages
+{
+	partial class Index : ComponentBase
+	{
+		public IEnumerable<Image> ImageList { get; set; } = new List<Image>();
+	}
+}

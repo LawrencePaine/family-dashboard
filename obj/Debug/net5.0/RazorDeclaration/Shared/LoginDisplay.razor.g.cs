2a7cd0d6902ec636d5dec9bc2fbd0794719c613d
DrummerBoi95 <40663257+DrummerBoi95@users.noreply.github.com>
2021-08-17 15:05:51 +0100

    adds login display and authentication

--- obj/Debug/net5.0/RazorDeclaration/Shared/LoginDisplay.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/LoginDisplay.razor.g.cs
@@ -13,71 +13,78 @@ namespace Family_Dashboard.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\lpain\family-dashboard\_Imports.razor"
+#line 1 "C:\Users\lpain\Documents\GitHub\family-dashboard\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\lpain\family-dashboard\_Imports.razor"
+#line 2 "C:\Users\lpain\Documents\GitHub\family-dashboard\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\lpain\family-dashboard\_Imports.razor"
-using Microsoft.AspNetCore.Components.Authorization;
-
-#line default
-#line hidden
-#nullable disable
-#nullable restore
-#line 4 "C:\Users\lpain\family-dashboard\_Imports.razor"
+#line 4 "C:\Users\lpain\Documents\GitHub\family-dashboard\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\lpain\family-dashboard\_Imports.razor"
+#line 5 "C:\Users\lpain\Documents\GitHub\family-dashboard\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\lpain\family-dashboard\_Imports.razor"
+#line 6 "C:\Users\lpain\Documents\GitHub\family-dashboard\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\lpain\family-dashboard\_Imports.razor"
+#line 7 "C:\Users\lpain\Documents\GitHub\family-dashboard\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\lpain\family-dashboard\_Imports.razor"
+#line 8 "C:\Users\lpain\Documents\GitHub\family-dashboard\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\lpain\family-dashboard\_Imports.razor"
+#line 9 "C:\Users\lpain\Documents\GitHub\family-dashboard\_Imports.razor"
 using Family_Dashboard;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\lpain\family-dashboard\_Imports.razor"
+#line 10 "C:\Users\lpain\Documents\GitHub\family-dashboard\_Imports.razor"
 using Family_Dashboard.Shared;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 1 "C:\Users\lpain\Documents\GitHub\family-dashboard\Shared\LoginDisplay.razor"
+using Microsoft.AspNetCore.Components.Authorization;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "C:\Users\lpain\Documents\GitHub\family-dashboard\Shared\LoginDisplay.razor"
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 #line default
 #line hidden
@@ -89,6 +96,20 @@ using Family_Dashboard.Shared;
         {
         }
         #pragma warning restore 1998
+#nullable restore
+#line 17 "C:\Users\lpain\Documents\GitHub\family-dashboard\Shared\LoginDisplay.razor"
+      
+    private async Task BeginSignOut(MouseEventArgs args)
+    {
+        await SignOutManager.SetSignOutState();
+        Navigation.NavigateTo("authentication/logout");
+    }
+
+#line default
+#line hidden
+#nullable disable
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private SignOutSessionStateManager SignOutManager { get; set; }
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private NavigationManager Navigation { get; set; }
     }
 }
 #pragma warning restore 1591
